6f9ab8a2dce96a60dbde38c88a75872638941f6a
jhonnyrave <jhonnyrave09@hotmail.com>
2021-03-21 14:32:57 -0500

    Initial file

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php 
+    include 'bd/BD.php';
+    header('Access-Control-Allow-Origin:*');
+    if ($_SERVER["REQUEST_METHOD"] == 'GET'){
+        if(isset($_GET['id'])){
+            $query="select * from frameworks where id=".$_GET['id'];
+            $resultado=metodoGet($query);
+            echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
+        } else {
+            $query="select * from frameworks";
+            $resultado=metodoGet($query);
+            echo json_encode($resultado->fetchAll());
+        }
+        header("HTTP/1.1 200 OK");
+        exit();
+    }
+
+    if ($_POST["METHOD"] == 'POST'){
+        unset($_POST["METHOD"]);
+        $nombre = $_POST['nombre'];
+        $lanzamiento = $_POST['lanzamiento'];
+        $desarrollador= $_POST['desarrollador'];
+        $query = "insert into frameworks(nombre,lanzamiento, desarrollador) 
+        values ('$nombre','$lanzamiento', '$desarrollador')";
+        $queryAutoIncrement = "select max(id) as id from frameworks";
+        $resultado = metodoPost($query, $queryAutoIncrement);
+        echo json_encode($resultado);
+        header("HTTP/1.1 200 OK");
+        exit();
+    }
+
+    if ($_POST["METHOD"] == 'PUT'){
+        unset($_POST["METHOD"]);
+        $id = $_GET['id'];
+        $nombre = $_POST['nombre'];
+        $lanzamiento = $_POST['lanzamiento'];
+        $desarrollador= $_POST['desarrollador'];
+        $query = "update frameworks set nombre = '$nombre', 
+        lanzamiento = '$lanzamiento', desarrollador = '$desarrollador' 
+        where id = '$id'";
+        $resultado = metodoPut($query);
+        echo json_encode($resultado);
+        header("HTTP/1.1 200 OK");
+        exit();
+    }
+
+    if ($_POST["METHOD"] == 'DELETE'){
+        unset($_POST["METHOD"]);
+        $id = $_GET['id'];
+        $query = "DELETE from frameworks where id = '$id'";
+        $resultado = metodoDelete($query);
+        echo json_encode($resultado);
+        header("HTTP/1.1 200 OK");
+        exit();
+    }
+    header("HTTP/1.1 400 BAD REQUEST");
+?>
